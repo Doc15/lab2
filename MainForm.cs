318651e24ace78a2d33ec12ff7eaf4d1093fdbb6
Doc15 <54259761+Doc15@users.noreply.github.com>
2021-02-26 17:54:50 +0700

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Project1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = " SELECT author.name as Author, book.name as Book, genre.name as Genre FROM book JOIN author ON book.id_autore = author.ID JOIN genre on book.id_genre = genre.ID; ";
+
+            MySqlConnection conn = Class2.GetDbConnection();
+            MySqlCommand cmDB = new MySqlCommand(query, conn);
+            cmDB.CommandTimeout = 60;
+            MySqlDataReader rd;
+
+            try
+            {
+                conn.Open();
+                rd = cmDB.ExecuteReader();
+                if (rd.HasRows)
+                {
+                    while(rd.Read())
+                    {
+                        string[] row = { rd.GetString(0), rd.GetString(1), rd.GetString(2) };
+                        var listViewItem = new ListViewItem(row);
+                        listView1.Items.Add(listViewItem);
+                    }
+                }
+                conn.Close();
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                MessageBox.Show("Подключение прошло успешно");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения");
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
